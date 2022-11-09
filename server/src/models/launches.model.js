390a9e4be062544d1008d5cb979c1347a2701555
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-09 12:40:06 +0100

    feat: implement the addNewLaunch functionality

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,11 @@
 const launches = new Map();
-
+let latestFlightNumber = 100;
 
 const launch= {
     flightNumber: 100,
     mission : 'Kepler Exploration X',
     rocket: 'Explorer ISI',
-    launchDate: 'December 17, 2030',
+    launchDate: new Date('December 17, 2030'),
     target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
@@ -29,6 +29,19 @@ function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch){
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, {
+        upcoming: true,
+        success: true,
+        customers: ["Zero to Mastery", 'NASA'],
+        flightNumber: latestFlightNumber,
+    }))
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { httpGetAllLaunches } = require('./launches.controller');
+const { httpGetAllLaunches, httpAddNewLaunches } = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunches);
 
 module.exports = launchesRouter; 
