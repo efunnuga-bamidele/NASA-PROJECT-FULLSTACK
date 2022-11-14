725e19d02a911d36b8adfa289103a743b37ff78b
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-14 08:42:44 +0100

    Implement Get Launches test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,10 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
+    test('It should respond with 200 success', async () => {
+        const response = await request(app).get('/launches');
+        expect(response.statusCode).toBe(200);
     })
 })
 
@@ -13,6 +16,6 @@ describe('Text POST /launch', () => {
 
     })
     test('It should catch invalid dates', () => {
-        
+
     })
 })
